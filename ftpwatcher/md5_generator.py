29a54e4c859710163dc7278957834e7f78c25fe7
dietervanhoof <dieter.vanhoof@c4j.be>
2015-11-26 12:06:58 +0100

    Fixed MD5 check not being in HEX format

--- ftpwatcher/md5_generator.py
+++ ftpwatcher/md5_generator.py
@@ -11,6 +11,6 @@ def generate_md5(file_path):
         with open(file_path,'rb') as f:
             for chunk in iter(lambda: f.read(128 * md5.block_size), b''):
                 md5.update(chunk)
-        return md5.digest()
+        return md5.hexdigest()
     except Exception as ex:
         logging.error("Could not generate md5 for " + file_path + "(" + type(ex).__name__ + ")")
