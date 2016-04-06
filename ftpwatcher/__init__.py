b23ac82f1b7d29e8caa45075c604bedfe22b1495
Tina <cochet.tina@telenet.be>
2016-04-06 15:38:28 +0200

    Update __init__.py
    disable scanner

--- ftpwatcher/__init__.py
+++ ftpwatcher/__init__.py
@@ -1,9 +1,9 @@
-from ftpwatcher import directory_scanner
+#from ftpwatcher import directory_scanner
 from ftpwatcher.fileindex import FileIndex
 from ftpwatcher import directory_watcher
 
 
 def watch_folder(folder_path, config):
     file_index = FileIndex(config)
-    directory_scanner.scan(folder_path, file_index)
+    #directory_scanner.scan(folder_path, file_index)
     directory_watcher.watch(folder_path, file_index)
