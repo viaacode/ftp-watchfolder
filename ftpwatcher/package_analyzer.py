16d95f95fa09d8d421ddd07cda5a5413f51dcf65
Jonas Liekens <jonas.liekens@c4j.be>
2016-04-27 16:39:59 +0200

    Added more analyzer logging

--- ftpwatcher/package_analyzer.py
+++ ftpwatcher/package_analyzer.py
@@ -8,7 +8,7 @@ def loop(file_index, config):
     not_gonna_give_you_up = True
     max_nr_of_checks = int(config['CHECK_PACKAGE_AMOUNT'])
     while not_gonna_give_you_up:
-        logging.info("Checking file_index on completed packages...")
+        logging.info('Checking file_index on completed packages...')
         for index_name in file_index.packages.keys():
             try:
                 package = file_index.packages.get(index_name)
@@ -25,6 +25,7 @@ def loop(file_index, config):
                     logging.info('Package {} is still incomplete. Check {} of {}'.format(index_name, package.times_checked, max_nr_of_checks))
             except Exception as ex:
                 logging.error('Checking file_index failed: {}'.format(str(ex)))
+        logging.info('{} incomplete packages remaining...'.format(len(file_index.packages)))
         time.sleep(int(config['CHECK_PACKAGE_INTERVAL']))
 
 
