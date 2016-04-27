88e2aa20dd6299e03c8b00ae67f7151ca4e17b2f
Jonas Liekens <jonas.liekens@c4j.be>
2016-04-27 15:53:14 +0200

    Moved general exception handling in analyzer

--- ftpwatcher/package_analyzer.py
+++ ftpwatcher/package_analyzer.py
@@ -8,9 +8,9 @@ def loop(file_index, config):
     not_gonna_give_you_up = True
     max_nr_of_checks = int(config['CHECK_PACKAGE_AMOUNT'])
     while not_gonna_give_you_up:
-        try:
-            logging.info("Checking file_index on completed packages...")
-            for index_name in file_index.packages.keys():
+        logging.info("Checking file_index on completed packages...")
+        for index_name in file_index.packages.keys():
+            try:
                 package = file_index.packages.get(index_name)
                 if is_package_complete(package, config):
                     logging.info('Package \'{}\' complete.'.format(index_name))
@@ -21,9 +21,8 @@ def loop(file_index, config):
                 package.increment_times_checked()
                 logging.info('Package {} is still incomplete. Check {} of {}'.format(index_name, package.times_checked,
                                                                                      max_nr_of_checks))
-        except Exception as ex:
-            logging.error('Checking file_index failed: {}'.format(str(ex)))
-
+            except Exception as ex:
+                logging.error('Checking file_index failed: {}'.format(str(ex)))
         time.sleep(int(config['CHECK_PACKAGE_INTERVAL']))
 
 
