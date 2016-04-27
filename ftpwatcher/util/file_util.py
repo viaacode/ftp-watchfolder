34ca34604bbc3efa77fcdc40e647503fa0adc98e
Jonas Liekens <jonas.liekens@c4j.be>
2016-04-27 16:04:08 +0200

    Fixed Issue #4

--- ftpwatcher/util/file_util.py
+++ ftpwatcher/util/file_util.py
@@ -4,9 +4,9 @@ import logging
 
 def move_file(package, folder):
     for file_entry in package.files:
-        new_path = file_entry.get('file_path') + "/" + folder
-        source = file_entry.get('file_path') + "/" + file_entry.get('file_name')
-        destination = new_path + "/" + file_entry.get('file_name')
+        new_path = os.path.join(file_entry.get('file_path'), folder)
+        source = os.path.join(file_entry.get('file_path'), file_entry.get('file_name'))
+        destination = os.path.join(new_path, file_entry.get('file_name'))
         logging.info("Moving '{}' to '{}'.".format(source, destination))
         os.rename(
                 source,
