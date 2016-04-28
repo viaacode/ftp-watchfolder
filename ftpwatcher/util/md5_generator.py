eac6dedb1d4fd343a3689db8be241baa27cc99b1
Jonas Liekens <jonas.liekens@c4j.be>
2016-04-28 09:14:06 +0200

    Merge branch 'watcher_rework' into development

--- ftpwatcher/util/md5_generator.py
+++ ftpwatcher/util/md5_generator.py
@@ -2,14 +2,13 @@ import logging
 import json
 from json import loads, dumps
 import pika
-from ftpwatcher import rabbit_connector as rabbit
+from ftpwatcher.util import rabbit_connector as rabbit
 from pika.credentials import PlainCredentials
 
 import pdb
 
-__author__ = 'viaa'
 
-class Md5_generator:
+class MD5Generator:
     def __init__(self, file_index):
         self.file_index = file_index
         connection = pika.BlockingConnection(pika.ConnectionParameters(
@@ -20,13 +19,13 @@ class Md5_generator:
 
         channel = connection.channel()
         channel.queue_declare(queue=self.file_index.config['RABBIT_MQ_MD5_RESPONSE_QUEUE'], durable=True)
-        channel.basic_consume(self.callback, self.queue)
+        channel.basic_consume(self.callback, self.file_index.config['RABBIT_MQ_MD5_RESPONSE_QUEUE'])
         channel.start_consuming()
 
     def generate_md5(self, file_path, file_name):
         pdb.set_trace()
         logging.info("Sending md5 message...")
-        rabbit.send_message(
+        connection = rabbit.Connector(
             host=self.file_index.config['RABBIT_MQ_HOST'],
             port=int(self.file_index.config['RABBIT_MQ_PORT']),
             username=self.file_index.config['RABBIT_MQ_USER'],
@@ -34,7 +33,9 @@ class Md5_generator:
             exchange=self.file_index.config['RABBIT_MQ_MD5_REQUEST_EXCHANGE'],
             topic_type=self.file_index.config['RABBIT_MQ_TOPIC_TYPE'],
             queue=self.file_index.config['RABBIT_MQ_MD5_REQUEST_QUEUE'],
-            routing_key=self.file_index.config['FLOW_ID'],
+            routing_key=self.file_index.config['FLOW_ID']
+        )
+        connection.send_message(
             message=json.dumps({
                 "correlation_id": "1",
                 "server": "",
