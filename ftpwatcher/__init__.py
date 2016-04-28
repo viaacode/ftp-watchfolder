84f8f00080d819a2e636a56f2d17b23d6f1fe543
Jonas Liekens <jonas.liekens@c4j.be>
2016-04-28 08:19:31 +0200

    Removed watcher connector params

--- ftpwatcher/__init__.py
+++ ftpwatcher/__init__.py
@@ -2,6 +2,6 @@ from ftpwatcher.fileindex import FileIndex
 from ftpwatcher import directory_watcher
 
 
-def watch_folder(folder_path, config, rabbit_connector):
+def watch_folder(folder_path, config):
     file_index = FileIndex(config)
     directory_watcher.watch(folder_path, file_index, config)
