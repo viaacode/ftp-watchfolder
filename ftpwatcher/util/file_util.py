5451b2946739254085781f7e3959fa67c9ac87c2
Jonas Liekens <jonas.liekens@c4j.be>
2016-04-28 10:48:31 +0200

    Added check on fileutil for files that are already in the proper folder

--- ftpwatcher/util/file_util.py
+++ ftpwatcher/util/file_util.py
@@ -4,14 +4,17 @@ import logging
 
 def move_file(package, folder):
     for file_entry in package.files:
-        new_path = os.path.join(file_entry.get('file_path'), folder)
         source = os.path.join(file_entry.get('file_path'), file_entry.get('file_name'))
+        new_path = os.path.join(file_entry.get('file_path'), folder)
         destination = os.path.join(new_path, file_entry.get('file_name'))
         logging.info("Moving '{}' to '{}'.".format(source, destination))
-        os.rename(
-                source,
-                destination
-        )
-        file_entry.update({'file_path': new_path})
-        logging.info("File moved.")
+        if folder not in source:
+            os.rename(
+                    source,
+                    destination
+            )
+            file_entry.update({'file_path': new_path})
+            logging.info("File moved.")
+        else:
+            logging.info("File {} has already been moved to {}!".format(file_entry.get('file_name'), new_path))
     pass
